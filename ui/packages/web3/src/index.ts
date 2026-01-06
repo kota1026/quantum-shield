7f1433479ffd03931b32db46d8ee10cc808c084e
kota1026 <108375319+kota1026@users.noreply.github.com>
2026-01-06 09:19:59 +0900

    feat(web3): Export contracts config and new SIWE types
    - Export contracts, validateContractAddresses
    - Export SessionStorageType, UseSIWEOptions
    - Export isChainSupported, sepolia, aegisL3

--- ui/packages/web3/src/index.ts
+++ ui/packages/web3/src/index.ts
@@ -1,16 +1,17 @@
 // Config
 export { wagmiConfig, chains } from './config/wagmi';
-export { CHAIN_IDS, SUPPORTED_CHAINS, getChainConfig } from './config/chains';
+export { CHAIN_IDS, SUPPORTED_CHAINS, getChainConfig, isChainSupported, sepolia, aegisL3 } from './config/chains';
+export { contracts, validateContractAddresses, type ContractName } from './config/contracts';
 
 // Providers
 export { Web3Provider } from './providers/web3-provider';
 
 // Hooks
-export { useSIWE, type SIWESession } from './hooks/use-siwe';
+export { useSIWE, type SIWESession, type SessionStorageType, type UseSIWEOptions } from './hooks/use-siwe';
 export { useQSLock } from './hooks/use-qs-lock';
 export { useQSUnlock } from './hooks/use-qs-unlock';
 export { useQSStatus } from './hooks/use-qs-status';
 
-// Contracts
+// Contracts (legacy - prefer using contracts config)
 export { QS_VAULT_ABI, QS_VAULT_ADDRESS } from './contracts/qs-vault';
 export { QS_STAKING_ABI, QS_STAKING_ADDRESS } from './contracts/qs-staking';
